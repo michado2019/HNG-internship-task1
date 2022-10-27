7290e3e5aa52d41bf20efcf01a47323889988285
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-10-27 14:31:00 +0100

    profile component updated with profile image and the twitter username

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Profile.css'
-
+import profileImg from '../images/profile-pix.jpg'
 export default function Profile() {
   return (
-    <div>
-       profile
+    <div className='profileWrapper'>
+            <img src={profileImg} alt='profileImg' id='profile_img'/>
+            <h1 id='twitter'>Mike_Adeshina</h1>
+            <h2 id='slack'>michado2019</h2>
     </div>
   )
 }
