ba3caa180e8369cb78db638ae409ce63594c82e8
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-10-27 15:32:17 +0100

    profile component modified by adding a menu icon

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import './Profile.css'
-import profileImg from '../images/profile-pix.jpg'
+import React from "react";
+import "./Profile.css";
+import profileImg from "../images/profile-pix.jpg";
+import { MoreHorizOutlined } from "@mui/icons-material";
+
 export default function Profile() {
   return (
-    <div className='profileWrapper'>
-            <img src={profileImg} alt='profileImg' id='profile_img'/>
-            <h1 id='twitter'>Mike_Adeshina</h1>
-            <h2 id='slack'>michado2019</h2>
+    <div className="profileWrapper">
+      <MoreHorizOutlined className='profileMenu-icon'/>
+      <div className="profileFlex">
+        <img src={profileImg} alt="profileImg" id="profile_img" />
+        <h1 id="twitter">Mike_Adeshina</h1>
+        <h2 id="slack">michado2019</h2>
+      </div>
     </div>
-  )
+  );
 }
