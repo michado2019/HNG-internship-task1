2f6336aff8aae400adba395d36b2e8e88af03ced
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-10-28 12:29:44 +0100

    link section completed with several links added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import LinkSection from "./components/LinkSection";
 import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
       <Profile />
+      <LinkSection />
     </div>
   );
 }
---
+++ src/components/LinkSection.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./LinkSection.css";
+
+export default function LinkSection() {
+  return (
+    <div className="linkSection-wrapper">
+      <a
+        href="https://twitter.com/Mike_Adeshina"
+        id="btn__twitter"
+        className="ZuriLinks"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Twitter Link
+      </a>
+      <a
+        href="https://training.zuri.team/"
+        id="btn__zuri"
+        className="ZuriLinks"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Zuri Team
+      </a>
+      <a
+        href="http://books.zuri.team"
+        id="books"
+        className="ZuriLinks"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Zuri Books
+      </a>
+      <a
+        href="https://books.zuri.team/python-for-beginners?ref_id=michado21"
+        id="book_python"
+        className="ZuriLinks"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Python Book
+      </a>
+      <a
+        href="https://background.zuri.team"
+        id="pitch"
+        className="ZuriLinks"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Background Check for Coders
+      </a>
+      <a
+        href="https://books.zuri.team/design-rules"
+        id="book_design"
+        className="ZuriLinks"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Design Books
+      </a>
+    </div>
+  );
+}
