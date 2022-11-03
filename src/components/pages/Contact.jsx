aeaf8782289e1864e9767d2e49f57e0e6397a5c1
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-03 17:54:20 +0100

    style: css styling apply to className titleDetails in the Contact.jsx

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -7,7 +7,6 @@ export default function Contact() {
       <h1 className='contactTitle'>Contact Me</h1>
       <p className='contactDetails'>Hi there, contact me to ask me about anything you have in mind.</p>
     <form className='contactForm'>
-    
     </form>
     </div>
   )
