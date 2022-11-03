f8ae855c13dee210ac6fe9c7717371d7683d3f18
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-03 18:40:01 +0100

    feat: inputs added to the form

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import './Contact.css'
+import React from "react";
+import "./Contact.css";
 
 export default function Contact() {
   return (
-    <div className='contactWrapper'>
-      <h1 className='contactTitle'>Contact Me</h1>
-      <p className='contactDetails'>Hi there, contact me to ask me about anything you have in mind.</p>
-    <form className='contactForm'>
-    </form>
+    <div className="contactWrapper">
+      <h1 className="contactTitle">Contact Me</h1>
+      <p className="contactDetails">
+        Hi there, contact me to ask me about anything you have in mind.
+      </p>
+      <form className="contactForm">
+        <input
+          type="text"
+          placeholder="Enter your first name"
+          className="contactInput"
+          id="first_name"
+        />
+        <input
+          type="text"
+          placeholder="Enter your last name"
+          className="contactInput"
+          id="last_name"
+        />
+        <input
+          type="text"
+          placeholder="yourname@gmail.com"
+          className="contactInput"
+          id="email"
+        />
+      </form>
     </div>
-  )
+  );
 }
