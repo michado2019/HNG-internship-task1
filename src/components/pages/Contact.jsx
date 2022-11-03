2d2d46556d880b3bea5ac354c028fc5b30087c82
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-03 17:48:03 +0100

    feat: form tag added to the Contact

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -4,7 +4,11 @@ import './Contact.css'
 export default function Contact() {
   return (
     <div className='contactWrapper'>
-          contact
+      <h1 className='contactTitle'>Contact Me</h1>
+      <p className='contactDetails'>Hi there, contact me to ask me about anything you have in mind.</p>
+    <form className='contactForm'>
+    
+    </form>
     </div>
   )
 }
