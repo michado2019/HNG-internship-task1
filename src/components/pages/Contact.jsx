33d69fced121522c1833355196d52040d853500d
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-04 02:49:20 +0100

    style: Styles added to form fields in Contact.jsx

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -10,32 +10,41 @@ export default function Contact() {
         Hi there, contact me to ask me about anything you have in mind.
       </p>
       <form className="contactForm">
-        <label htmlFor="first_name">Form name</label>
+        <label htmlFor="first_name" className='contactForm-labels'>First name</label>
         <input
           type="text"
           placeholder="Enter your first name"
           className="contactInput"
           id="first_name"
         />
-        <label htmlFor="Last_name">Last name</label>
+        <label htmlFor="Last_name" className='contactForm-labels'>Last name</label>
         <input
           type="text"
           placeholder="Enter your last name"
           className="contactInput"
           id="last_name"
         />
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email" className='contactForm-labels'>Email</label>
         <input
           type="text"
           placeholder="yourname@gmail.com"
           className="contactInput"
           id="email"
         />
-        <label htmlFor="message">Message</label>
-        <textarea placeholder="Send me a message and I&#39;ll reply as soon as possible" className="contactInput" id='message' />
-        <label htmlFor="checkbox">You agree to providing your data to <span id='name'>{name}</span> who may contact you.</label>
+        <label htmlFor="message" className='contactForm-labels'>Message</label>
+        <textarea
+          placeholder="Send me a message and I&#39;ll reply as soon as possible"
+          className="contactInput"
+          id="message"
+        />
         <input type="checkbox" id="checkbox" name="" value="" />
-        <button className="contactInput" id='btn__submit'>Send message</button>
+        <label htmlFor="checkbox" className='contactForm-labels' id='text_area'>
+          You agree to providing your data to <span id="name">{name}</span> who
+          may contact you.
+        </label>
+        <button className="contactInput" id="btn__submit">
+          Send message
+        </button>
       </form>
     </div>
   );
